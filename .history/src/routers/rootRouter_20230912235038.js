7f7eebdbf7318aa64515ddd3ef8ce0891a699250
MIJI-H <mijiheo3@gmail.com>
2023-09-12 23:53:06 +0900

    Login

--- .history/src/routers/rootRouter_20230912235038.js
+++ .history/src/routers/rootRouter_20230912235038.js
@@ -12,7 +12,7 @@ const globalRouter = express.Router();
 
 rootRouter.get("/", home);
 rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.route("/login").get(getLogin).post(postLogin);
+rootRouter.get("/login", login);
 rootRouter.get("/search", search);
 
 export default rootRouter;
